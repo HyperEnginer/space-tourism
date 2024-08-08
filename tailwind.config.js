6a63eb7d8666e86d74fa7c0cd032a46328fe6c7a
Biruk Bizuayehu <brookbizuayehu@gmail.com>
2024-08-08 14:38:54 +0300

    add bg image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-
     fontFamily: {
-      'barlow':"'Barlow', sans-serif",
-      "barlow-condensed":"'Barlow Condensed', sans-serif",
-      "bellefair":"'Bellefair', serif"
+      barlow: "'Barlow', sans-serif",
+      "barlow-condensed": "'Barlow Condensed', sans-serif",
+      bellefair: "'Bellefair', serif",
     },
     extend: {
       colors: {
-        "white":"#fff"
+        white: "#fff",
       },
       backdropBlur: {
-        '4xl': '80px',
-      }
+        "4xl": "80px",
+      },
     },
     screens: {
-      "tablet": { max: "768px" },
-      "mobile": { max: "375px" },
+      tablet: { max: "768px" },
+      mobile: { max: "375px" },
+    },
+    backgroundImage: {
+      "destination-mobile":
+        "url('/src/assets/destination/background-destination-mobile.jpg')",
+      "destination-tablet":
+        "url('/src/assets/destination/background-destination-tablet.jpg')",
+      "destination-desktop":
+        "url('/src/assets/destination/background-destination-desktop.jpg')",
     },
   },
   plugins: [],
-}
-
+};
